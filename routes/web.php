f8eb90162bcd0ca8c929f9a9391b4df1eefd8dfb
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2022-04-10 12:31:36 +0100

    update to the project

--- routes/web.php
+++ routes/web.php
@@ -18,10 +18,18 @@ use App\Models\User;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('https://huntintel.io/');
 });
 
 Route::middleware(['auth:sanctum', 'verified', 'subscribed'])->get('/dashboard', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified', 'subscribed'])->get('/knowledge-base', function () {
+    return view('dashboard');
+})->name('dashboard');
+
+Route::middleware(['auth:sanctum', 'verified', 'subscribed'])->get('/location-tools', function () {
     return view('dashboard');
 })->name('dashboard');
 
