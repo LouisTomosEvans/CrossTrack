fb040f8cb755e48b0e2e55e18a524d3a74f36232
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2022-04-13 14:45:47 +0100

    adding tracking

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -14,6 +14,31 @@
     <meta name="msapplication-TileColor" content="#44d62c">
     <meta name="theme-color" content="#ffffff">
     <title>Dashboard | Hunt Intelligence</title>
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-3QQ05DYWT3"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-3QQ05DYWT3');
+    </script>
+    <!-- Meta Pixel Code -->
+    <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '503294348131030');
+    fbq('track', 'PageView');
+    </script>
+    <noscript><img height="1" width="1" style="display:none"
+    src="https://www.facebook.com/tr?id=503294348131030&ev=PageView&noscript=1"
+    /></noscript>
+    <!-- End Meta Pixel Code -->
     </head>
     <body>
     <div id="app">
