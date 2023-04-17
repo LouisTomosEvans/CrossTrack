618b919d6a115586a39116807a70b18b49e72cfc
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2023-04-17 19:52:36 +0100

    Update CompaniesAPIService.php

--- app/Services/Companies/CompaniesAPIService.php
+++ app/Services/Companies/CompaniesAPIService.php
@@ -20,10 +20,10 @@ class CompaniesAPIService
 
         if(!empty($additionalParams)) {
             $additionalParams = http_build_query($additionalParams);
-            $response = $this->client->get("/companies/{$domain}?token={$apiKey}&{$additionalParams}");
+            $response = $this->client->get("/v1/companies/{$domain}?token={$apiKey}&{$additionalParams}");
         } else {
             $additionalParams = '';
-            $response = $this->client->get("/companies/{$domain}&token={$apiKey}");
+            $response = $this->client->get("/companies/{$domain}?token={$apiKey}");
         }
 
         return json_decode($response->getBody()->getContents(), true);
