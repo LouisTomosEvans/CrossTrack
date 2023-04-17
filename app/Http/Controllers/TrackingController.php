7fad056bb0423f29f8339b631e4c73e040291c3e
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2023-04-17 20:10:56 +0100

    Update TrackingController.php

--- app/Http/Controllers/TrackingController.php
+++ app/Http/Controllers/TrackingController.php
@@ -273,6 +273,7 @@ class TrackingController extends Controller
               //  Data Enrichment 2 - CompaniesAPI
               $companiesAPI = new CompaniesAPIService();
               $companiesAPIData = $companiesAPI->getCompanyByDomain($company['domain']);
+              dump($companiesAPIData);
               if ($companiesAPIData) {
                 // get logo
                 $company['logo'] = $companiesAPIData['logo'] ?? null;
