4c6101c68db42c7d8ce05c28df62c4764466e84a
Louis Tomos Evans <LouisTomosEvans@protonmail.com>
2023-04-20 17:09:49 +0100

    update

--- app/Http/Controllers/CompanyLeadsController.php
+++ app/Http/Controllers/CompanyLeadsController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Visits;
+use Illuminate\Support\Facades\DB;
 
 class CompanyLeadsController extends Controller
 {
@@ -27,13 +29,14 @@ class CompanyLeadsController extends Controller
         foreach ($leads as $lead) {
             $lead->website_name = $lead->website->name;
             $lead->website_favicon = $lead->website->favicon;
-            $lead->visit_count = $lead->visits->count();
+            //  get number of sessions, a session is a visit by a user that last 30 minutes, after 30 mins its a new session
+            $lead->sessions = $lead->visits->groupBy('visitor_id')->count();
             $lead->last_seen = $lead->visits->sortByDesc('timestamp')->first()->timestamp;
             $lead->location = $lead->city . ', ' . $lead->country;
             $lead->unique_visitors = $lead->visits->unique('visitor_id')->count();
             $lead->total_duration = $lead->visits->sum('session_duration');
             // rand score for now
-            $lead->lead_score = rand(1, 100);
+            $lead->lead_score = $lead->leadScore->score;
             // top pages visited
             $lead->top_pages = $lead->visits->groupBy('url')->map(function ($item) {
                 return $item->count();
@@ -47,6 +50,20 @@ class CompanyLeadsController extends Controller
                 $user->name = $user->name;
                 return $user;
             });
+
+            $sessionTimeout = 30;
+            $sessionTimeoutSeconds = $sessionTimeout * 60;
+
+            $rawQuery = "SELECT COUNT(*) as session_count
+                        FROM (
+                            SELECT visitor_id, timestamp,
+                                LAG(timestamp) OVER (PARTITION BY visitor_id ORDER BY timestamp) as prev_timestamp
+                            FROM visits
+                            WHERE company_leads_id = ?
+                        ) AS subquery
+                        WHERE (TIMESTAMPDIFF(MINUTE, subquery.timestamp, subquery.prev_timestamp) > ?) OR (subquery.prev_timestamp IS NULL)";
+
+            $lead->visit_count = DB::selectOne($rawQuery, [$lead->id, $sessionTimeoutSeconds])->session_count;
 
         }
 
